4610cec7d52e6089c4033dd7b67c127ec063d6ce
Ratmir Mukazhanov <ratmir.m2004@gmail.com>
2023-12-16 16:53:58 +0000

    Trying to migrate front end into server side

--- src/main/java/com/example/demo_spring/User/UserService.java
+++ src/main/java/com/example/demo_spring/User/UserService.java
@@ -7,35 +7,36 @@ import java.util.List;
 @Service
 public class UserService {
 
-        private final UserRepository userRepository;
+    private final UserRepository userRepository;
 
-        public UserService(UserRepository userRepository) {
-            this.userRepository = userRepository;
-        }
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-        public User getUser(Long id) {
-            return userRepository.findById(id).orElse(null);
-        }
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
 
-        public List<User> getAllUsers() {
-            return userRepository.findAll();
-        }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
-        public void addUser(User user) {
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public void updateUser(Long id, User updatedUser) {
+        User user = userRepository.findById(id).orElse(null);
+        if (user != null) {
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
             userRepository.save(user);
         }
-
-        public void deleteUser(Long id) {
-            userRepository.deleteById(id);
-        }
-
-        public void updateUser(Long id, User updatedUser) {
-            User user = userRepository.findById(id).orElse(null);
-            if (user != null) {
-                user.setName(updatedUser.getName());
-                user.setEmail(updatedUser.getEmail());
-                user.setPassword(updatedUser.getPassword());
-                userRepository.save(user);
-            }
-        }
+    }
+    
 }
