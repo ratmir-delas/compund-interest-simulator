a8e683b1226b71b6801db2497c7b61f1098918a1
Ratmir Mukazhanov <ratmir.m2004@gmail.com>
2023-12-16 20:38:19 +0000

    Cosmetic changes

--- src/main/java/com/example/demo_spring/User/User.java
+++ src/main/java/com/example/demo_spring/User/User.java
@@ -16,21 +16,27 @@ public class User {
     private String name;
     private String email;
     private String password;
+    private String county;
+    private String currency;
 
     public User() {
     }
 
-    public User(String name, String email, String password) {
+    public User(String name, String email, String password, String county, String currency) {
         this.name = name;
         this.email= email;
         this.password = password;
+        this.county = county;
+        this.currency = currency;
     }
 
-    public User(Long id, String name, String email, String password) {
+    public User(Long id, String name, String email, String password, String county, String currency) {
         this.id = id;
         this.name = name;
         this.email= email;
         this.password = password;
+        this.county = county;
+        this.currency = currency;
     }
 
     public Long getId() {
@@ -65,17 +71,33 @@ public class User {
         this.password = password;
     }
 
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(password, user.password) && Objects.equals(county, user.county) && Objects.equals(currency, user.currency);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, email, password);
+        return Objects.hash(id, name, email, password, county, currency);
     }
 
     @Override
@@ -85,7 +107,8 @@ public class User {
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
                 ", password='" + password + '\'' +
+                ", county='" + county + '\'' +
+                ", currency='" + currency + '\'' +
                 '}';
     }
-
 }
