cdd05afaca2f5f1d831627e81aa2fbe2a09f3f14
Ratmir Mukazhanov <ratmir.m2004@gmail.com>
2023-12-21 20:12:11 +0000

    Project structure alteration

--- src/main/java/com/prospero/simulator/repositories/UserRepository.java
+++ src/main/java/com/prospero/simulator/repositories/UserRepository.java
@@ -1,5 +1,6 @@
-package com.prospero.simulator.User;
+package com.prospero.simulator.repositories;
 
+import com.prospero.simulator.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
